98e5add390fa50ed2966be7d37152399c90e9200
Zax Rosenberg <zax.rosenberg@gmail.com>
2018-09-20 02:28:10 -0500

    Add meetup module to pull valid name list for venue

---
+++ project_night/meetup/meetup.py
@@ -0,0 +1,66 @@
+import os
+
+import meetup.api
+import numpy as np
+import pandas as pd
+from natsort import humansorted, ns
+
+
+MEETUP_API_KEY = os.getenv('MEETUP_API_KEY', None)
+
+
+def get_valid_attendee_names(event_id=None, urlname='_ChiPy_', client=None):
+    client = client if client else meetup.api.Client(MEETUP_API_KEY)
+    event_id = event_id if event_id else get_summary_event_list(client=client)[0]['id']
+
+    rsvps = client.GetRsvps(event_id=event_id, urlname=urlname)
+
+    attendees = [{'Name': person['member']['name'],
+                  'RSVP': person['response'],
+                  'Given Name': person['answers'][0]}
+                 for person in rsvps.results]
+
+    df = pd.DataFrame(attendees)
+    df = df[df['RSVP'] != 'no']
+    df['Security Name'] = df['Given Name'].replace('', np.nan).combine_first(df['Name'])
+
+    # Only keep rows where there's at least a first and last name
+    mask = [True if len(x) > 1 else False for x in df['Security Name'].str.findall(r'\w+')]
+    df = df[mask]
+
+    # Human sort the names
+    df['Security Name'] = humansorted(df['Security Name'].tolist(), alg=ns.IGNORECASE)
+
+    return df['Security Name']
+
+
+def get_summary_event_list(client=None):
+    client = client if client else meetup.api.Client(MEETUP_API_KEY)
+
+    events = client.GetEvents(group_urlname='_ChiPy_')
+
+    # todo time needs to be formatted into a readable form
+    return [{'name': e['name'], 'time': e['time'], 'id': e['id'], 'event_url': e['event_url']}
+            for e in events.results]
+
+
+# # To match the csv output for event attendees, use:
+    # rsvps = client.GetRsvps(event_id=event_id, urlname=urlname)
+    # member_ids = [person['member']['member_id'] for person in rsvps.results]
+    # members = client.GetMembers(member_id=member_ids)
+    # mem_id_to_link = [{m['id']: m['link']}for m in members.results]
+
+#     attendees = []
+#     for person in rsvps.results:
+#         d = {'Name': person['member']['name'],
+#              # 'User ID': person['member']['member_id'],
+#              # 'Title': ,
+#              # 'Event Host': ,
+#              'RSVP': person['response'],
+#              # 'Guests': person['guests'],
+#              # 'RSVPed on': ,
+#              # 'Joined Group on': person['mtime'] ?
+#              # 'URL of Member Profile': mem_id_to_link[person['member']['member_id']]
+#              'Given Name': person['answers'][0]}
+
+#         attendees.append(d)
--- setup.py
+++ setup.py
@@ -5,34 +5,37 @@
 from setuptools import setup, find_packages
 
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 # list of strings specifying what other distributions need to be present in order for the setup script to run
-setup_requires = ['pytest-runner']
+setup_requires = ["pytest-runner"]
 
 # ut package requirements here
-install_requires = ["pandas",
+install_requires = ["meetup-api",
+                    "natsort",
+                    "numpy",
+                    "pandas",
                     "prompt_toolkit"]
 
-tests_require = ['pytest']
+tests_require = ["pytest"]
 
 setup(
-    name='project_night',
+    name="project_night",
     version='0.1.0',
     description="Utilities for ChiPy's Project Night",
     long_description=readme,
-    url='https://github.com/zaxr/project_night',
+    url="https://github.com/zaxr/project_night",
     author="Zax Rosenberg",
-    author_email='zaxr@protonmail.com',
-    classifiers=['Development Status :: 3 - Alpha',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-                 'Natural Language :: English',
-                 'Programming Language :: Python :: 3.6'],
-    packages=find_packages(exclude=['docs', 'tests']),
+    author_email="zaxr@protonmail.com",
+    classifiers=["Development Status :: 3 - Alpha",
+                 "Intended Audience :: Developers",
+                 "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                 "Natural Language :: English",
+                 "Programming Language :: Python :: 3.6"],
+    packages=find_packages(exclude=["docs", "tests"]),
     setup_requires=setup_requires,
     install_requires=install_requires,
     tests_require=tests_require,
-    test_suite='tests',
+    test_suite="tests",
 )
